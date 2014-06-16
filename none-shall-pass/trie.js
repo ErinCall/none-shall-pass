c14f3dbff70f7eea53f65647f0d2d41ed7ed4905
Andrew Lorente <hello@andrewlorente.com>
2014-06-16 12:56:15 -0700

    CommonJS modules don't need IIFEs
    Thanks @saikobee!

--- none-shall-pass/trie.js
+++ none-shall-pass/trie.js
@@ -1,62 +1,60 @@
-module.exports = (function() {
-    var lookup = function(head, rest) {
+var lookup = function(head, rest) {
+    if (rest.length === 0) {
+        return true;
+    } else if (head in this._trie) {
+        return this._trie[head].has(rest);
+    } else {
+        return false;
+    }
+};
+
+
+var Trie = function Trie(){
+    this._trie = {};
+
+    this.add = function(word) {
+        var head = word.slice(0,1),
+            rest = word.slice(1);
+
+        if (typeof this._trie[head] === 'undefined') {
+            this._trie[head] = new this.constructor();
+        }
+
         if (rest.length === 0) {
-            return true;
-        } else if (head in this._trie) {
-            return this._trie[head].has(rest);
+            return;
         } else {
-            return false;
+            this._trie[head].add(rest);
         }
     };
 
-
-    var Trie = function Trie(){
-        this._trie = {};
-
-        this.add = function(word) {
-            var head = word.slice(0,1),
-                rest = word.slice(1);
-
-            if (typeof this._trie[head] === 'undefined') {
-                this._trie[head] = new this.constructor();
-            }
-
-            if (rest.length === 0) {
-                return;
-            } else {
-                this._trie[head].add(rest);
-            }
-        };
-
-        this.has = function(word) {
-            var head = word.slice(0,1),
-                rest = word.slice(1);
-            return lookup.call(this, head, rest);
-        };
-
-        this.addMany = function(words) {
-            for (var i=0; i < words.length; i++) {
-                this.add(words[i]);
-            }
-        };
+    this.has = function(word) {
+        var head = word.slice(0,1),
+            rest = word.slice(1);
+        return lookup.call(this, head, rest);
     };
 
-    var LeetTrie = function LeetTrie() {
-        Trie.call(this);
-        this.has = function(word) {
-            var head = word.slice(0,1),
-                rest = word.slice(1);
-
-            if (head === '0') {
-                head = 'o';
-            }
-
-            return lookup.call(this, head, rest);
-        };
+    this.addMany = function(words) {
+        for (var i=0; i < words.length; i++) {
+            this.add(words[i]);
+        }
     };
+};
 
-    return {
-        'Trie': Trie,
-        'LeetTrie': LeetTrie
+var LeetTrie = function LeetTrie() {
+    Trie.call(this);
+    this.has = function(word) {
+        var head = word.slice(0,1),
+            rest = word.slice(1);
+
+        if (head === '0') {
+            head = 'o';
+        }
+
+        return lookup.call(this, head, rest);
     };
-})();
+};
+
+module.exports = {
+    'Trie': Trie,
+    'LeetTrie': LeetTrie
+};
